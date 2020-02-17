ebc0506cfb77a647dfd2d95f7f58ce448434f735
wangweimin <wang0.618@qq.com>
2020-02-17 21:50:51 +0800

    修改打包配置

--- setup.py
+++ setup.py
@@ -9,6 +9,14 @@ setup(
     author_email='wangweimin@buaa.edu.com',
     license='MIT',
     packages=find_packages(),
+    package_data={
+        # data files need to be listed both here (which determines what gets
+        # installed) and in MANIFEST.in (which determines what gets included
+        # in the sdist tarball)
+        "wsrepl": [
+            "html",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",

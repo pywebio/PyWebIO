d111b47e4e889d122d8623ef5d12a623b75fd549
wangweimin <wang0.618@qq.com>
2021-01-20 20:09:58 +0800

    doc update

--- pywebio/__version__.py
+++ pywebio/__version__.py
@@ -1,5 +1,5 @@
 __package__ = 'pywebio'
-__description__ = 'Write web app in script way.'
+__description__ = 'Write interactive web app in script way.'
 __url__ = 'https://pywebio.readthedocs.io'
 __version__ = "1.0.1"
 __version_info__ = (1, 0, 1, 0)

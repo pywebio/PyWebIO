pywebio/PyWebIO

commit 0f505504a4b07d993bb3c81fd660edf61a37e64d

maint: change django backend starting method

pywebio/platform/django.py
@@ -205,11 +205,17 @@ def start_server(applications, port=8080, host='localhost',
         path(r'<path:path>', serve, {'document_root': STATIC_PATH}),
     ]
 
-    get_wsgi_application()
+    app = get_wsgi_application()  # load app
 
     has_coro_target = any(iscoroutinefunction(target) or isgeneratorfunction(target) for
                           target in make_applications(applications).values())
     if not disable_asyncio and has_coro_target:
         threading.Thread(target=run_event_loop, daemon=True).start()
 
-    call_command('runserver', '%s:%d' % (host, port))
+    # call_command('runserver', '%s:%d' % (host, port))
+    # or use below code to run web app
+    import tornado.wsgi
+    container = tornado.wsgi.WSGIContainer(app)
+    http_server = tornado.httpserver.HTTPServer(container)
+    http_server.listen(port, address=host)
+    tornado.ioloop.IOLoop.current().start()

commit 949bb3c748459e5f5ae8261f7beffbba552f362a

fix django server error (introduced in 0f50550) in coro-session

pywebio/platform/django.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import threading
-from functools import partial
+import os
 
 from django.http import HttpResponse, HttpRequest
 
@@ -182,17 +182,20 @@ def start_server(applications, port=8080, host='localhost',
         path(r'<path:path>', serve, {'document_root': STATIC_PATH}),
     ]
 
-    app = get_wsgi_application()  # load app
+    use_tornado_wsgi = os.environ.get('PYWEBIO_DJANGO_WITH_TORNADO', True)
+    if use_tornado_wsgi:
+        app = get_wsgi_application()  # load app
 
-    has_coro_target = any(iscoroutinefunction(target) or isgeneratorfunction(target) for
-                          target in make_applications(applications).values())
-    if has_coro_target:
-        threading.Thread(target=run_event_loop, daemon=True).start()
+        import tornado.wsgi
+        container = tornado.wsgi.WSGIContainer(app)
+        http_server = tornado.httpserver.HTTPServer(container)
+        http_server.listen(port, address=host)
+        tornado.ioloop.IOLoop.current().start()
+    else:
+        from django.core.management import call_command
+        has_coro_target = any(iscoroutinefunction(target) or isgeneratorfunction(target) for
+                              target in make_applications(applications).values())
+        if has_coro_target:
+            threading.Thread(target=run_event_loop, daemon=True).start()
 
-    # call_command('runserver', '%s:%d' % (host, port))
-    # or use below code to run web app
-    import tornado.wsgi
-    container = tornado.wsgi.WSGIContainer(app)
-    http_server = tornado.httpserver.HTTPServer(container)
-    http_server.listen(port, address=host)
-    tornado.ioloop.IOLoop.current().start()
+        call_command('runserver', '%s:%d' % (host, port))

test/14.django_multiple_session_impliment.py
@@ -7,7 +7,6 @@ import template
 import time
 import util
 from pywebio.input import *
-from pywebio.output import *
 from pywebio.utils import to_coroutine, run_as_function
 
 
@@ -52,9 +51,8 @@ def start_test_server():
     global urlpatterns
 
     pywebio.enable_debug()
-    import threading
     from functools import partial
-    from pywebio.platform.django import webio_view, run_event_loop
+    from pywebio.platform.django import webio_view
     from django.conf import settings
     from django.core.wsgi import get_wsgi_application
     from django.urls import path
@@ -99,8 +97,6 @@ def start_test_server():
     ]
 
     app = get_wsgi_application()  # load app
-
-    threading.Thread(target=run_event_loop, daemon=True).start()
 
     import tornado.wsgi
     container = tornado.wsgi.WSGIContainer(app)
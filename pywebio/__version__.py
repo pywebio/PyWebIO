pywebio/PyWebIO

commit 3df460a44a122238c9ac71974f33615ac1e06dba

Persistent input feature (#109)
* maint(frontend): refine `InputItem`

* feat: persistent input -> `pin` 🎉

* fix wrong value type in `put_input(type='number'/'float')`

* add pin doc and its translate

* test: add pin module test

webiojs/src/handlers/input.ts
@@ -38,7 +38,7 @@ export class InputHandler implements CommandHandler {
         if (old_ctrls)
             old_ctrls[old_ctrls.length - 1].after_show();
 
-        if(state.AutoFocusOnInput)
+        if (state.AutoFocusOnInput)
             $('[auto_focus="true"]').focus();
     };
 
@@ -198,7 +198,17 @@ class FormController {
             if (!(input_spec.type in FormController.input_items))
                 throw new Error(`Unknown input type '${input_spec.type}'`);
             let item_class = FormController.input_items[input_spec.type];
-            let item = new item_class(this.session, this.task_id, input_spec);
+            let item = new item_class(input_spec, this.spec.task_id, (event, input_item) => {
+                this.session.send_message({
+                    event: "input_event",
+                    task_id: this.task_id,
+                    data: {
+                        event_name: event,
+                        name: input_spec.name,
+                        value: input_item.get_value()
+                    }
+                });
+            });
             this.name2input[input_spec.name] = item;
 
             body.append(item.create_element());

webiojs/src/models/input/input.ts
@@ -1,5 +1,4 @@
 import {InputItem} from "./base";
-import {Session} from "../../session";
 import {deep_copy} from "../../utils"
 import {state} from "../../state";
 
@@ -29,16 +28,17 @@ const common_input_tpl = `
 
 
 export class Input extends InputItem {
-    static accept_input_types: string[] = ["text", "password", "number", "color", "date", "range", "time", "email", "url"];
+    static accept_input_types: string[] = ["text", "password", "number", "float", "color", "date", "range", "time", "email", "url"];
 
-    constructor(session: Session, task_id: string, spec: any) {
-        super(session, task_id, spec);
+    constructor(spec: any, task_id: string, on_input_event: (event_name: string, input_item: InputItem) => void) {
+        super(spec, task_id, on_input_event);
     }
 
     create_element(): JQuery {
         let spec = deep_copy(this.spec);
         const id_name = spec.name + '-' + Math.floor(Math.random() * Math.floor(9999));
         spec['id_name'] = id_name;
+        if (spec['type'] == 'float') spec['type'] = 'text';
 
         let html = Mustache.render(common_input_tpl, spec);
 
@@ -54,16 +54,16 @@ export class Input extends InputItem {
         });
 
         let input_elem = this.element.find('input');
-        if(spec.onblur){
+        if (spec.onblur) {
             // blur事件时，发送当前值到服务器
             input_elem.on("blur", (e) => {
-                if(this.get_value())
-                    this.send_value_listener(this, e)
+                if (this.get_value())
+                    this.on_input_event("blur", this);
             });
         }
-        if(spec.onchange){
+        if (spec.onchange) {
             input_elem.on("input", (e) => {
-                this.send_value_listener(this, e, 'change');
+                this.on_input_event("change", this);
             });
         }
 
@@ -89,18 +89,22 @@ export class Input extends InputItem {
 
     update_input(spec: any): any {
         let attributes = spec.attributes;
-        if('datalist' in attributes){
+        if ('datalist' in attributes) {
             const datalist_html = Mustache.render(datalist_tpl, {datalist: attributes.datalist});
             this.element.find('datalist').empty().append(datalist_html);
             delete attributes['datalist'];
         }
 
-
         this.update_input_helper(-1, attributes);
     }
 
     get_value(): any {
-        return this.element.find('input').val();
+        let val= this.element.find('input').val();
+        if(this.spec['type']=='number')
+            val = parseInt(val as string);
+        else if (this.spec['type']=='float')
+            val = parseFloat(val as string);
+        return val;
     }
 }
 

demos/__init__.py
@@ -3,4 +3,5 @@ r"""
 .. automodule:: demos.input_usage
 .. automodule:: demos.output_usage
 .. automodule:: demos.chat_room
+.. automodule:: demos.markdown_previewer
 """

demos/__main__.py
@@ -19,6 +19,7 @@ index_md = r"""### Basic demo
 
  - [BMI calculation](./bmi): Calculating Body Mass Index based on height and weight
  - [Online chat room](./chat_room): Chat with everyone currently online
+ - [Online chat room](./markdown_previewer): The online markdown editor with live preview
  - [Input demo](./input_usage): Demonstrate various input usage supported by PyWebIO
  - [Output demo](./output_usage): Demonstrate various output usage supported by PyWebIO
 

demos/doc_demo.py
@@ -8,6 +8,7 @@ from os import path, listdir
 from pywebio import start_server
 from pywebio.output import *
 from pywebio.input import *
+from pywebio.pin import *
 from pywebio.platform import seo
 from pywebio.session import *
 from pywebio.session import local as session_local
@@ -47,6 +48,7 @@ def run_code(code, scope):
 IMPORT_CODE = """from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import *
+from pywebio.pin import *
 
 """
 

demos/markdown_previewer.py
@@ -1,0 +1,34 @@
+"""
+Markdown Live Preview
+^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:demo_host:`Demo </markdown_previewer>`, `Source code <https://github.com/wang0618/PyWebIO/blob/dev/demos/markdown_previewer.py>`_
+"""
+from pywebio import start_server
+
+from pywebio.output import *
+from pywebio.pin import *
+from pywebio.session import set_env, download
+
+
+def main():
+    """Markdown Previewer"""
+    set_env(output_animation=False)
+
+    put_markdown("""# Markdown Live Preview
+    The online markdown editor with live preview. The source code of this application is [here](https://github.com/wang0618/PyWebIO/blob/dev/demos/markdown_previewer.py).
+    ## Write your Markdown
+    """, lstrip=True)
+    put_textarea('md_text', rows=18, code=True)
+
+    put_buttons(['Download content'], lambda _: download('saved.md', pin.md_text.encode('utf8')), small=True)
+
+    put_markdown('## Preview', sanitize=False)
+    while True:
+        change_detail = pin_wait_change('md_text')
+        with use_scope('md', clear=True):
+            put_markdown(change_detail['value'])
+
+
+if __name__ == '__main__':
+    start_server(main, port=8080, debug=True, cdn=False)

docs/locales/zh_CN/LC_MESSAGES/pin.po
@@ -1,0 +1,245 @@
+# SOME DESCRIPTIVE TITLE.
+# Copyright (C) WangWeimin
+# This file is distributed under the same license as the PyWebIO package.
+# FIRST AUTHOR <EMAIL@ADDRESS>, 2021.
+#
+msgid ""
+msgstr ""
+"Project-Id-Version: PyWebIO 1.2.3\n"
+"Report-Msgid-Bugs-To: \n"
+"POT-Creation-Date: 2021-05-28 18:47+0800\n"
+"PO-Revision-Date: 2021-05-28 18:47+0800\n"
+"Last-Translator: WangWeimin <wang0.618@qq.com>\n"
+"Language: zh_CN\n"
+"Language-Team: \n"
+"Plural-Forms: nplurals=1; plural=0;\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=utf-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"Generated-By: Babel 2.8.0\n"
+"X-Generator: Poedit 2.4.2\n"
+
+#: of pywebio.pin:2
+msgid "`pin` == Persistent input == Pinning input widget to the page"
+msgstr ""
+
+#: of pywebio.pin:5
+msgid "Overview"
+msgstr ""
+
+#: of pywebio.pin:7
+msgid ""
+"As you already know, the input function of PyWebIO is blocking and the input form will be destroyed after "
+"successful submission. In most cases, it enough to use this way to get input. However in some cases, you may "
+"want to make the input form **not** disappear after submission, and can continue to receive input."
+msgstr ""
+"我们已经知道，PyWebIO中的输入函数是阻塞式的，输入表单会在成功提交后被销毁。在大多数场景下，使用这种方式接收用"
+"户输入已经够用了。但在一些场景下，你或许希望输入表单在提交后不消失，并且可以继续接收输入。"
+
+#: of pywebio.pin:13
+msgid "So PyWebIO provide the ``pin`` module to achieve persistent input by pinning input widgets to the page."
+msgstr "所以，PyWebIO提供了 ``pin`` 模块来实现持续性输入。"
+
+#: of pywebio.pin:15
+msgid "The ``pin`` module achieves persistent input in 3 parts:"
+msgstr "``pin`` 模块主要有3部分内容:"
+
+#: of pywebio.pin:17
+msgid ""
+"First, this module provides some pin widgets. Pin widgets are not different from output widgets in ``pywebio."
+"output`` module, besides that they can also receive input."
+msgstr ""
+"首先， ``pin`` 模块提供了一些 pin 组件(widgets)。Pin 组件和 ``pywebio.output`` 模块中的输出组件并没有什么不"
+"同，只不过它还可以接收输入。"
+
+#: of pywebio.pin:21
+msgid "This code outputs an text input pin widget:"
+msgstr "以下代码输出了一个最基本的文本框pin组件:"
+
+#: of pywebio.pin:23
+msgid "put_input('input', label='This is a input widget')"
+msgstr ""
+
+#: of pywebio.pin:29
+msgid ""
+"In fact, the usage of pin widget function is same as the output function. You can use it as part of the "
+"combined output, or you can output pin widget to a scope:"
+msgstr ""
+"实际上, pin 组件函数的调用方式和输出函数是一致的，你可以将其作为组合输出的一部分, 也可以将其输出到某个scope中:"
+
+#: of pywebio.pin:32
+msgid ""
+"put_row([\n"
+"    put_input('input'),\n"
+"    put_select('select', options=['A', 'B', 'C'])\n"
+"])\n"
+"\n"
+"with use_scope('search-area'):\n"
+"    put_input('search', placeholder='Search')"
+msgstr ""
+
+#: of pywebio.pin:44
+msgid "Then, you can use the `pin` object to get the value of pin widget:"
+msgstr "然后，你可以使用 `pin` 对象来获取pin组件的值："
+
+#: of pywebio.pin:46
+msgid ""
+"put_input('pin_name')\n"
+"put_buttons(['Get Pin Value'], lambda _: put_text(pin.pin_name))"
+msgstr ""
+
+#: of pywebio.pin:53
+msgid ""
+"The first parameter that the pin widget function receives is the name of the pin widget. You can get the "
+"current value of the pin widget via the attribute of the same name of the `pin` object."
+msgstr ""
+"Pin组件函数的第一个参数为pin组件的 ``name`` 。你可以使用 `pin` 对象的同名属性来获取对应pin组件的当前值。"
+
+#: of pywebio.pin:56
+msgid ""
+"In addition, the `pin` object also supports getting the value of the pin widget by index, that is to say::"
+msgstr "另外， `pin` 对象同样支持以索引的方式获取pin组件的值，即::"
+
+#: of pywebio.pin:58
+msgid "pin['pin_name'] == pin.pin_name"
+msgstr ""
+
+#: of pywebio.pin:60
+msgid "There are also two useful functions when you use the pin module: `pin_wait_change()` and `pin_update()`."
+msgstr "Pin模块中还有两个有用的函数: `pin_wait_change()` 和 `pin_update()`。"
+
+#: of pywebio.pin:62
+msgid ""
+"Since the pin widget functions is not blocking, `pin_wait_change()` is used to wait for the value of one of a "
+"list of pin widget to change, it 's a blocking function."
+msgstr ""
+"由于pin组件输出函数是非阻塞的， 所以使用 `pin_wait_change()` 来等待一组pin组件的值发生变化，它是一个阻塞式函"
+"数。"
+
+#: of pywebio.pin:65
+msgid "`pin_update()` can be used to update attributes of pin widgets."
+msgstr "`pin_update()` 可以用来更新pin组件的输出属性。"
+
+#: of pywebio.pin:68
+msgid "Pin widgets"
+msgstr ""
+
+#: of pywebio.pin:69
+msgid "Each pin widget function corresponds to an input function of :doc:`input <./input>` module."
+msgstr "每个pin组件函数都对应一个 :doc:`input <./input>` 模块中的输入函数。"
+
+#: of pywebio.pin:71
+msgid "The function of pin widget supports most of the parameters of the corresponding input function."
+msgstr "Pin组件函数支持大多数对应的输入函数的参数。"
+
+#: of pywebio.pin:73
+msgid "The following is the difference between the two in parameters:"
+msgstr "以下为两者之间的一些不同:"
+
+#: of pywebio.pin:75
+msgid ""
+"The first parameter of pin widget function is always the name of the widget, and the name needs to be unique "
+"throughout the session."
+msgstr "Pin组件函数的第一个参数始终是Pin组件的 ``name`` ，且 ``name`` 需要在整个会话中保持唯一。"
+
+#: of pywebio.pin:77
+msgid ""
+"Pin functions don't support the ``on_change`` and ``validate`` callbacks, and the ``required`` parameter."
+msgstr "Pin组件函数不支持 ``on_change`` 、 ``validate`` 回调，也不支持 ``required`` 参数。"
+
+#: of pywebio.pin:78
+msgid "Pin functions have additional ``scope`` and ``position`` parameters for output control."
+msgstr "Pin组件函数多了用于输出控制的 ``scope`` 和 ``position`` 参数。"
+
+#: of pywebio.pin.put_input:1
+msgid "Output an input widget. Refer to: `pywebio.input.input()`"
+msgstr "输出一个文本输入组件。参见 `pywebio.input.input()`"
+
+#: of pywebio.pin.put_textarea:1
+msgid "Output a textarea widget. Refer to: `pywebio.input.textarea()`"
+msgstr "输出一个文本域输入组件。参见 `pywebio.input.textarea()`"
+
+#: of pywebio.pin.put_select:1
+msgid "Output a select widget. Refer to: `pywebio.input.select()`"
+msgstr "输出一个下拉选择输入组件。参见 `pywebio.input.select()`"
+
+#: of pywebio.pin.put_checkbox:1
+msgid "Output a checkbox widget. Refer to: `pywebio.input.checkbox()`"
+msgstr "输出一个多选框组件。参见 `pywebio.input.checkbox()`"
+
+#: of pywebio.pin.put_radio:1
+msgid "Output a radio widget. Refer to: `pywebio.input.radio()`"
+msgstr "输出一个单选按钮组件。参见 `pywebio.input.radio()`"
+
+#: of pywebio.pin:87
+msgid "Pin utils"
+msgstr ""
+
+#: of pywebio.pin:90
+msgid "Pin widgets value getter and setter."
+msgstr "获取与设置Pin组件的值"
+
+#: of pywebio.pin:92
+msgid ""
+"You can use attribute or key index of ``pin`` object to get the current value of a pin widget. When accessing "
+"the value of a widget that does not exist, it returns ``None`` instead of throwing an exception."
+msgstr ""
+"你可以使用属性或索引来从 ``pin`` 对象上获取pin组件的当前值。当访问不存在的pin组件时，会得到 ``None`` 而不会抛"
+"出异常。"
+
+#: of pywebio.pin:95
+msgid "You can also use the ``pin`` object to set the value of pin widget:"
+msgstr "``pin`` 对象还支持设置pin组件的值:"
+
+#: of pywebio.pin:97
+msgid ""
+"put_input('counter', type='number', value=0)\n"
+"\n"
+"while True:\n"
+"    pin.counter = pin.counter + 1  # Equivalent to: pin['counter'] = pin['counter'] + 1\n"
+"    time.sleep(1)"
+msgstr ""
+
+#: of pywebio.pin.pin_wait_change:1
+msgid ""
+"``pin_wait_change()`` listens to a list of pin widgets, when the value of any widgets changes, the function "
+"returns with the name and value of the changed widget."
+msgstr ""
+"``pin_wait_change()`` 监听一组pin组件，当其中任意一个的值发生变化后，函数返回发生变化的组件的 ``name`` 和值。"
+
+#: of pywebio.pin.pin_update pywebio.pin.pin_wait_change
+msgid "Parameters"
+msgstr ""
+
+#: of pywebio.pin.pin_wait_change:4
+msgid "List of names of pin widget"
+msgstr "Pin组件 ``name`` 列表"
+
+#: of pywebio.pin.pin_wait_change
+msgid "return dict"
+msgstr ""
+
+#: of pywebio.pin.pin_wait_change:7
+msgid ""
+":demo_host:`Here </markdown_previewer>` is an demo of using `pin_wait_change()` to make a markdown previewer."
+msgstr ":demo_host:`这里 </markdown_previewer>` 有一个使用 `pin_wait_change()` 实现markdown实时预览的demo。"
+
+#: of pywebio.pin.pin_wait_change:9
+msgid ""
+"Note that: updating value with the :data:`pin` object or `pin_update()` does not trigger `pin_wait_change()` "
+"to return"
+msgstr "注：使用 :data:`pin` 对象或 `pin_update()` 更新pin组件的值不会引发 `pin_wait_change()` 返回"
+
+#: of pywebio.pin.pin_update:1
+msgid "Update attributes of pin widgets."
+msgstr "更新pin组件的输出属性。"
+
+#: of pywebio.pin.pin_update:3
+msgid "The ``name`` of the target input widget."
+msgstr "目标pin组件的 ``name`` 。"
+
+#: of pywebio.pin.pin_update:4
+msgid ""
+"The pin widget parameters need to be updated. Note that those parameters can not be updated: ``type``, "
+"``name``, ``code``, ``multiple``"
+msgstr "需要被更新的pin组件的参数。注意这些参数无法被更新： ``type``, ``name``, ``code``, ``multiple``"

commit 9f8c93653948be61c641bd78dab685e909be07d1

fix a bug introduced in commit `3df460a`

pywebio/__version__.py
@@ -1,8 +1,8 @@
 __package__ = 'pywebio'
 __description__ = 'Write interactive web app in script way.'
 __url__ = 'https://pywebio.readthedocs.io'
-__version__ = "1.3.1"
-__version_info__ = (1, 3, 1, 0)
+__version__ = "1.3.2"
+__version_info__ = (1, 3, 2, 0)
 __author__ = 'WangWeimin'
 __author_email__ = 'wang0.618@qq.com'
 __license__ = 'MIT'

webiojs/src/handlers/input.ts
@@ -198,7 +198,7 @@ class FormController {
             if (!(input_spec.type in FormController.input_items))
                 throw new Error(`Unknown input type '${input_spec.type}'`);
             let item_class = FormController.input_items[input_spec.type];
-            let item = new item_class(input_spec, this.spec.task_id, (event, input_item) => {
+            let item = new item_class(input_spec, this.task_id, (event, input_item) => {
                 this.session.send_message({
                     event: "input_event",
                     task_id: this.task_id,